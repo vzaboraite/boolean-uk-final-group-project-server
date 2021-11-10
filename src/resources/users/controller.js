62c5deef042655045ca4fdc96e8a0c9163d01f72
Rafael Armelin <raffael.armelin@gmail.com>
2021-11-10 13:16:11 +0000

    creating user and profile

--- src/resources/users/controller.js
+++ src/resources/users/controller.js
@@ -1,3 +1,34 @@
 const { prisma } = require("../../utils/db");
 
-module.exports = {};
+const createUserAndProfile = async (req, res) => {
+  const data = req.body;
+
+  console.log("data: ", data);
+
+  console.log("request: ", req);
+
+  const { name, profile } = data;
+  const { email, country } = profile;
+
+  try {
+    const result = await prisma.user.create({
+      data: {
+        name,
+        profile: {
+          create: { email, country },
+        },
+      },
+      include: {
+        profile: true,
+        projects: true,
+        donations: true,
+      },
+    });
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
+};
+
+module.exports = { createUserAndProfile };
--- src/resources/users/router.js
+++ src/resources/users/router.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+const { createUserAndProfile } = require("./controller");
+
 const router = express.Router();
+
+router.post("/", createUserAndProfile);
 
 module.exports = router;
