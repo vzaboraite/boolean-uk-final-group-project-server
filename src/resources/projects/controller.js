382904e39df4eb32491a48b8b348172d1467ebbe
Lana Ferrari <ferrarirangel@gmail.com>
2021-11-10 11:09:49 +0000

    updating data

--- src/resources/projects/controller.js
+++ src/resources/projects/controller.js
@@ -3,7 +3,7 @@ const { prisma } = require("../../utils/db");
 const getAllProjects = async (req, res) => {
   try {
     const result = await prisma.project.findMany();
-    res.json({ data: result });
+    res.json(result);
   } catch (error) {
     console.error({ error: error.message });
     res.status(500).json({ error: error.message });
