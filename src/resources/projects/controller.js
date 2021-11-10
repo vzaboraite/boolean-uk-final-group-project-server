2106c2183b4baa9a878670056b61cbb464bd6f2c
Lana Ferrari <ferrarirangel@gmail.com>
2021-11-10 11:00:47 +0000

    adding get all projects

--- src/resources/projects/controller.js
+++ src/resources/projects/controller.js
@@ -1,3 +1,14 @@
 const { prisma } = require("../../utils/db");
 
-module.exports = {};
+const getAllProjects = async (req, res) => {
+  try {
+    const result = await prisma.project.findMany();
+    res.json({ data: result });
+  } catch (error) {
+    console.error({ error: error.message });
+    res.status(500).json({ error: error.message });
+  }
+};
+module.exports = {
+  getAllProjects,
+};
