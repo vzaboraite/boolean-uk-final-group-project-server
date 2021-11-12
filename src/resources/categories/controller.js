eb2ef3fb33e98323d3a6442d9fb5ddb44b962d0d
Lana Ferrari <ferrarirangel@gmail.com>
2021-11-12 09:25:07 +0000

    adding getAllCategories

--- src/resources/categories/controller.js
+++ src/resources/categories/controller.js
@@ -1,3 +1,13 @@
 const { prisma } = require("../../utils/db");
 
-module.exports = {};
+const getAllCategories = async (req, res) => {
+  try {
+    const result = await prisma.category.findMany();
+    res.json(result);
+  } catch (error) {
+    console.error({ error: error.message });
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getAllCategories };
--- src/resources/categories/router.js
+++ src/resources/categories/router.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+const { getAllCategories } = require("./controller");
+
 const router = express.Router();
+
+router.get("/", getAllCategories);
 
 module.exports = router;
