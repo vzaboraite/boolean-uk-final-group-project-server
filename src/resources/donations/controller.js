56f763393e445b395b3a42688837e91765a1a6b7
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-11-12 10:25:51 +0000

    Build getAllDonations() controller

--- src/resources/donations/controller.js
+++ src/resources/donations/controller.js
@@ -1,3 +1,18 @@
 const { prisma } = require("../../utils/db");
 
-module.exports = {};
+const getAllDonations = async (req, res) => {
+  try {
+    const result = await prisma.donation.findMany({
+      include: {
+        user: true,
+        project: true,
+      },
+    });
+    res.json(result);
+  } catch (error) {
+    console.error({ error: error.message });
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getAllDonations };
--- src/resources/donations/router.js
+++ src/resources/donations/router.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { getAllDonations } = require("./controller");
 
 const router = express.Router();
+
+router.get("/", getAllDonations);
 
 module.exports = router;
